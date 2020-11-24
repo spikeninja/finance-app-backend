4a6fa18053a055dd7b2119dfc5272ace08f1f531
ArturSamvelyan <yourcamelcase@gmail.com>
2020-11-24 09:58:41 +0200

    db sql file created

---
+++ app/createdb.sql
@@ -0,0 +1,35 @@
+create table users(
+  id integer primary key,
+  name varchar(255),
+  email varchar(255),
+  password varchar(300),
+  created_at datetime
+);
+
+create table budget(
+  id integer primary key,
+  name varchar(255),
+  user_id integer,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+create table expense(
+  id integer primary key,
+  description text,
+  amount float,
+  category varchar(255),
+  created_at datetime,
+  budget_id integer,
+  FOREIGN KEY(budget_id) REFERENCES budget(id)
+);
+
+create table income(
+  id integer primary key,
+  description text,
+  amount float,
+  category varchar(255),
+  created_at datetime,
+  budget_id integer,
+  FOREIGN KEY(budget_id) REFERENCES budget(id)
+);
+
