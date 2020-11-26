8d88e8f88f340315cf8a741f1c92ce0c5af186e2
ArturSamvelyan <yourcamelcase@gmail.com>
2020-11-26 17:47:42 +0200

    Some functionality added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
+import uvicorn
 from fastapi import FastAPI
+
+from routes.users import router as users_router
 
 
 app = FastAPI()
+
+
+app.include_router(users_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
